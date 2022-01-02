c7a1751b87fa742eebbed713e7cd0f3d8b3b45cc
DrEremin <ivaneremin1979@gmail.com>
2022-01-02 23:03:18 +0300

    Initial commit

---
+++ src/main/java/ru/dreremin/internetbank/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.dreremin.internetbank;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InternetBankApplication.class);
+    }
+
+}
